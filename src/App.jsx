36bb25f80573e9cbb38cfab435cc05e4db8898f9
ViggaD <danhkvtse172932@gmail.com>
2024-10-29 14:25:45 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css";
-import BookingNotificationComponent from "./components/BookingNotification/BookingNotificationComponent";
 import AppRoutes from "./routes/AppRoutes";
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 function App() {
 
