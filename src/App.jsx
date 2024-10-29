64f5f8e9838e5b7a91eb0247101b56cf6cd8614b
ViggaD <danhkvtse172932@gmail.com>
2024-10-29 15:34:08 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
-import { WebSocketProvider } from "./services/config/provider/WebSocketProvider";
 
 function App() {
   return (
