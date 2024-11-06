425492c19fe9a6dfb07abeb254345e57ad74843e
ViggaD2003 <thanhdanhvt2003@gmail.com>
2024-11-06 22:37:45 +0700

    Update BookingCancel.jsx

--- src/components/Booking/BookingCancel.jsx
+++ src/components/Booking/BookingCancel.jsx
@@ -1,7 +1,7 @@
 const BookingCancel = () => {
     return (
       <div className="container mx-auto py-12 text-center">
-        <h1 className="text-3xl font-bold mb-4">Booking Successful!</h1>
+        <h1 className="text-3xl font-bold mb-4">Booking Cancel!</h1>
         <p className="text-gray-700 mb-6">
           Hope you booking our service again !
         </p>
